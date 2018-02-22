cbe79c53c5a031e6969c8cebe064024d697754e4
charumathivsb <31505787+charumathivsb@users.noreply.github.com>
2018-02-22 16:38:58 +0530

    Create numlmt.c

---
+++ numlmt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+              int num,n1,n2;
+             scanf("%d%d%d",&num,&n1,&n2);
+             if(num>=n1&&num<=n2)
+             printf("Yes");
+             else
+             printf("No");
+	// your code goes here
+	return 0;
+}
