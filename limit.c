fadd71d11c7c4eede8faa26486ac5c6d2d41854f
charumathivsb <31505787+charumathivsb@users.noreply.github.com>
2018-03-01 16:25:02 +0530

    Create limit.c

---
+++ limit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n,l,r;
+	scanf("%d %d %d",&n,&l,&r);
+            if((n>=l)&&(n<=r))
+            printf("yes");
+            else
+            printf("no");
+            return 0;
+	
+}
